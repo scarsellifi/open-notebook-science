ffe975db96557e046fd159a657d5e21eea6ac828
Scarselli Marco <marco.scarselli@kinoa.tech>
2018-10-21 11:24:05 +0200

    aggiunta sezione grafici monovariata

--- marradi.py
+++ marradi.py
@@ -22,12 +22,14 @@ def dist_frequenza(matrice, colonna, save = False, tipo = "categoriale" , lista_
     elif tipo == "ordinale": 
         try:
             distribuzione = distribuzione.reindex(lista_ordinale)
+            distribuzione = distribuzione.fillna(0)
             distribuzione["Cumulata"] = distribuzione["Percentuale"].cumsum()
             
             #distribuzione["cumsum"] = distribuzione[colonna].cumsum()
         except:
             try:
                 distribuzione = distribuzione.loc[lista_ordinale]
+                distribuzione = distribuzione.fillna(0)
                 distribuzione["Cumulata"] = distribuzione["Percentuale"].cumsum()
                 
             except:
@@ -36,18 +38,34 @@ def dist_frequenza(matrice, colonna, save = False, tipo = "categoriale" , lista_
 
     elif tipo == "cardinale":
         distribuzione.sort_index(inplace = True)
+        
+    try:
         distribuzione["Cumulata"] = distribuzione["Percentuale"].cumsum()
-        distribuzione["Cumulata"].loc[-1] = np.nan
-    
-    distribuzione.loc["Totale"] = distribuzione.apply(sum)
-    distribuzione["Percentuale"] = distribuzione["Percentuale"].round(2)
+        
+    except:
+        print("errore nella rimozione dell'incrocio Totale - Cumulata")    
 
+
+    distribuzione.loc["Totale"] = distribuzione.apply(sum)
+    
+    distribuzione["Percentuale"] = distribuzione["Percentuale"].round(2)
+    try:
+      distribuzione["Cumulata"] = distribuzione["Cumulata"].round(2)
+      distribuzione.loc["Totale", "Cumulata"] = ""
+    except:
+      pass
     
     if save == False:
         return distribuzione
     else:
         distribuzione.to_excel(str(save) + ".xlsx")
         return distribuzione
+
+def estrai_valore(cella):
+  try:
+    return int(cella[0])
+  except:
+    return cella
     
     
 def tabella_di_contingenza(dataframe, colonna_A, colonna_B, ordine_A = False, ordine_B = False, informativo = False):
@@ -71,3 +89,30 @@ def tabella_di_contingenza(dataframe, colonna_A, colonna_B, ordine_A = False, or
         crosstab = crosstab.applymap(str) + " " + expected.applymap(lambda x: ("( {:.2f})".format(x) )) + " " + (crosstab - expected).applymap(lambda x: ("( {:.2f})".format(x) ))
     
     return crosstab
+
+def plot_dist_frequenza(distribuzione, tipo = "categoriale", Y = "Percentuale", x_label="Valori", y_label="Percentuale"):
+  distribuzione = distribuzione.iloc[:-1, :]
+  
+  print(distribuzione)
+  if tipo == "categoriale":
+    distribuzione.index = distribuzione.index.map(lambda x: str(x))
+    g = sns.barplot(x = distribuzione.index, y=Y, data=distribuzione)
+    x = 0
+    for index, row in distribuzione.iterrows():
+      stringa = "N.{},\n {}%".format(row.Frequenze, row.Percentuale)
+      g.text(x,row.Frequenze, stringa, color='black', ha="center")
+      x = x + 1
+    g.set(xlabel=x_label, ylabel=y_label)
+  
+  elif tipo == "ordinale" or tipo == "cardinale":
+    index = distribuzione.index
+    distribuzione.reset_index(inplace = True)
+    g = sns.barplot(x = index, y=Y, data=distribuzione, palette="Blues_d")
+    for index, row in distribuzione.iterrows():
+      stringa = "F.{},\n {}%".format(row.Frequenze, row.Percentuale)
+      
+      g.text(row.name,row.Frequenze, stringa, color='black', ha="center")
+      g.set(xlabel=x_label, ylabel=y_label)
+  return g
+
+
